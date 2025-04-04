d9a9e735d5f4f33b8411de2db81edea6145f8665
Lillian <lchen45@charlotte.edu>
2025-04-04 15:34:03 -0400

    chapter 5

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,3 +1,9 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+// Any UI you add to the root layout will be shared across all pages in your application.
+// You can use the root layout to modify your <html> and <body> tags, and add metadata
+// the layout file is the best way to create a shared layout that all pages in your application can use.
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
